e067daf79a6cdd323292467a96c910be27c3e924
HASHEBUR RAHMAN <you@example.com>
2023-05-10 18:45:10 +0600

    added header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Header from './Component/Header/header'
+ 
+
+function App() {
+
+
+  return (
+    <div>
+       <Header></Header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/shop"> Shop</a>
+                <a href="/order"> Order Review</a>
+                <a href="/Manage Inventory">Manage Inventory</a>
+                <a href="/Login">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header; 
