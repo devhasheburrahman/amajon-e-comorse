c15c9f9ae2de0d256ef003c96e2e909041ee4213
HASHEBUR RAHMAN <you@example.com>
2023-05-13 00:00:43 +0600

    final-commit

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -4,11 +4,12 @@ const Cart = ({ cart }) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
-
+    let quantity = 0;
     for (const product of cart) {
-        totalPrice = totalPrice + product.price;
+        product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
-
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice * 7 / 100;
     const grandTotal = totalPrice + tax + totalShipping;
@@ -17,7 +18,7 @@ const Cart = ({ cart }) => {
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items:  {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice} </p>
             <p>Total Shipping Charge: ${totalShipping.toFixed(2)} </p>
             <p>Tax:$ {tax.toFixed(2)} </p>
